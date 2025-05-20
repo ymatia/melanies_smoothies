e28f7ca5084df06d89e73460450d0de6df4ebf1d
ymatia <ymatia@users.noreply.github.com>
2025-05-20 18:35:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,15 +12,18 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
 if ingredients_list:
     ingredients_string = ", ".join(ingredients_list)
     for fruit_chosen in ingredients_list:
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
+        st.subheader(f"{fruit_chosen} Nutrition Information')
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
 
     time_to_insert = st.button('Submit Order')
